d828eb9e9e7f27073baabb8b262d62df2d303a20
Jean <jean.olives@pucese.edu.ec>
2021-04-26 12:29:07 -0500

    Actualizando funcionalidad de nueva Factura.

--- Projects/front.grupoc.customevents.impl/src/main/java/com/cobiscorp/cobis/front/customevents/impl/view/executecommand/VA_VABUTTONECLWLDA_512877.java
+++ Projects/front.grupoc.customevents.impl/src/main/java/com/cobiscorp/cobis/front/customevents/impl/view/executecommand/VA_VABUTTONECLWLDA_512877.java
@@ -16,13 +16,19 @@ package com.cobiscorp.cobis.front.customevents.impl.view.executecommand;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.ReferenceCardinality;
 import org.apache.felix.scr.annotations.Service;
 import com.cobiscorp.cobis.commons.domains.log.ILogger;
 import com.cobiscorp.cobis.commons.log.LogFactory;
+import com.cobiscorp.cobis.front.model.Factura;
+import com.cobiscorp.designer.api.DataEntity;
+import com.cobiscorp.designer.api.DataEntityList;
 import com.cobiscorp.designer.api.DynamicRequest;
 import com.cobiscorp.designer.api.customization.IExecuteCommand;
 import com.cobiscorp.designer.api.customization.arguments.IExecuteCommandEventArgs;
 import com.cobiscorp.designer.api.managers.DesignerManagerException;
+import com.cobiscorp.designer.bli.api.IBLIExecutor;
 
 @Component
 @Service({ IExecuteCommand.class })
@@ -36,7 +42,23 @@ public class VA_VABUTTONECLWLDA_512877 implements IExecuteCommand {
 	 * Instancia de Logger
 	 */
 	private static final ILogger logger = LogFactory.getLogger(VA_VABUTTONECLWLDA_512877.class);
-
+	
+	@Reference(bind="setBliReservarFactura" , 
+			unbind="unsetBliReservarFactura",
+			cardinality = ReferenceCardinality.MANDATORY_UNARY,
+			target = "(bli.id=BLI8374_bli_reservarfactura)")
+	private IBLIExecutor bliReservarFactura;
+	
+	
+	private void setBliReservarFactura(IBLIExecutor bliMostrarProductos){
+		this.bliReservarFactura = bliMostrarProductos;
+	}
+	
+	private void unsetBliReservarFactura(IBLIExecutor bliMostrarProductos){
+		this.bliReservarFactura = null;
+	}
+	
+	
 	@Override
 	public void executeCommand(DynamicRequest arg0, IExecuteCommandEventArgs arg1) {
 		// TODO Auto-generated method stub
@@ -44,6 +66,13 @@ public class VA_VABUTTONECLWLDA_512877 implements IExecuteCommand {
 			if (logger.isDebugEnabled()) {
 				logger.logDebug("Start executeCommand in VA_VABUTTONECLWLDA_512877");
 			}
+			
+			bliReservarFactura.execute(arg0);
+			DataEntity factura = arg0.getEntity(Factura.ENTITY_NAME);
+			Integer codigo_cf = factura.get(Factura.CODIGO);
+			
+			logger.logDebug("codigo factura:" + codigo_cf);
+			
 		} catch (Exception ex) {
 			DesignerManagerException.handleException(arg1.getMessageManager(), ex, logger);
 		}
